c0f2526e7d16c121f247dfcf0dc8358a6e4337d6
Amit kumar <amitkrevolve@example.com>
2026-01-12 09:58:23 +0530

    requiremnets.txt

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="3ev_chatbot",
+    version="0.1.0",
+    author="Amit Kumar",
+    author_email="amitkrevolve@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
